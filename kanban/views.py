f224b76e91f0945a5ec5fd8ab0045cd81f649352
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2020-08-26 12:14:18 +0900

    :sparkles: ユーザー情報編集画面を作成

--- kanban/views.py
+++ kanban/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, resolve_url
+from django.views.generic import DetailView, UpdateView
+from .forms import UserFrom
+from .mixins import OnlyYouMixin
 
 # Create your views here.
 def index(request):
@@ -27,3 +31,16 @@ def signup(request):
     }
 
     return render(request, 'kanban/signup.html', context)
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = 'kanban/users/detail.html'
+
+
+class UserUpdateView(OnlyYouMixin, UpdateView):
+    model = User
+    template_name = 'kanban/users/update.html'
+    form_class = UserFrom
+
+    def get_success_url(self):
+        return resolve_url('kanban:users_detail', pk=self.kwargs['pk'])

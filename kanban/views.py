b558ba09d6657ac55bc5aea84773c51d5e81805c
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2020-08-26 10:07:33 +0900

    :sparkles: サインアップページの作成

--- kanban/views.py
+++ kanban/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def index(request):
     return render(request, 'kanban/index.html')
+
+def home(request):
+    return render(request, 'kanban/home.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user_instance = form.save()
+            login(request, user_instance)
+            return redirect('kanban:home')
+    else:
+        form = UserCreationForm()
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, 'kanban/signup.html', context)
